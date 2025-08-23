cf155a06a59d8fe044f35ca1079cde87c1107095
lohitakshay <lohitakshay2004@gmail.com>
2025-08-23 17:16:55 +0530

    thanks namaste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <div className="w-full max-w-full flex items-center justify-center">
       <motion.h2 className="text-center text-5xl mt-60 flex gap-5">
-        <motion.span initial={{y:20,opacity: 0}} animate={{y:0,opacity: 1}} transition={{duration: .3}}>Plan.</motion.span>
-        <motion.span initial={{y:20,opacity: 0}} animate={{y:0,opacity: 1}} transition={{duration: .3,delay:.4}}>Prioritize.</motion.span>
-        <motion.span initial={{y:20,opacity: 0}} animate={{y:0,opacity: 1}} transition={{duration: .3,delay: .8}}>Prosper.</motion.span>
+        <motion.span initial={{y:20,opacity: 0}} animate={{y:0,opacity: 1}} transition={{duration: .3}}>Hello.</motion.span>
+        <motion.span initial={{y:20,opacity: 0}} animate={{y:0,opacity: 1}} transition={{duration: .3,delay:.4}}>Namaste.</motion.span>
+        <motion.span initial={{y:20,opacity: 0}} animate={{y:0,opacity: 1}} transition={{duration: .3,delay: .8}}>Bye.</motion.span>
       </motion.h2>
 
     </div>
